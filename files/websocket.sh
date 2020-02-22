fe63fcb8e2decee418fd02d85fcdcf043e51d159
Gerard Ryan <G.M0N3Y.2503@gmail.com>
2020-02-22 18:25:54 +1000

    Fixed filenames and filename display

--- files/websocket.sh
+++ files/websocket.sh
@@ -2,9 +2,9 @@
 
 while :; do
     if read -r -s -t ${INPUT_TIMEOUT} INPUT; then
-        mega-get -m -q --ignore-quota-warn "'"${INPUT}"'" "${DOWNLOAD_DIR}/." > /dev/null || echo "Error parsing MEGA URL: '${INPUT}'"
+        mega-get -q --ignore-quota-warn "'"${INPUT}"'" "${DOWNLOAD_DIR}" > /dev/null || echo "Error parsing MEGA URL: '${INPUT}'"
     else
         echo "" # used to clear output
-        mega-transfers --limit=${TRANSFER_LIST_LIMIT}
+        mega-transfers --limit=${TRANSFER_LIST_LIMIT} --path-display-size=${PATH_DISPLAY_SIZE}
     fi
 done
